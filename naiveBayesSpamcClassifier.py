73c4a4850b395d124c6b73d3731f204fa85b50eb
Orkhan Suleymanov <53385236+orhancode@users.noreply.github.com>
2019-10-14 19:30:06 -0700

    Add files via upload

---
+++ naiveBayesSpamcClassifier.py
@@ -0,0 +1,55 @@
+import os
+import io
+import numpy
+from pandas import DataFrame
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+def readFiles(path):
+    for root, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            path = os.path.join(root, filename)
+
+            inBody = False
+            lines = []
+            f = io.open(path, 'r', encoding='latin1')
+            for line in f:
+                if inBody:
+                    lines.append(line)
+                elif line == '\n':
+                    inBody = True
+            f.close()
+            message = '\n'.join(lines)
+            yield path, message
+
+
+def dataFrameFromDirectory(path, classification):
+    rows = []
+    index = []
+    for filename, message in readFiles(path):
+        rows.append({'message': message, 'class': classification})
+        index.append(filename)
+
+    return DataFrame(rows, index=index)
+
+data = DataFrame({'message': [], 'class': []})
+
+data = data.append(dataFrameFromDirectory('emails/spam', 'spam'))
+data = data.append(dataFrameFromDirectory('emails/ham', 'ham'))
+
+
+#CountVectorizer to split up each message into its list of words
+#throw that into a MultinomialNB classifier
+vectorizer = CountVectorizer()
+counts = vectorizer.fit_transform(data['message'].values)
+
+classifier = MultinomialNB()
+targets = data['class'].values
+classifier.fit(counts, targets)
+
+
+# Testing
+examples = ['Free Protein Powder!!!', "Hi Mr Smith, could you confirm meeting time?"]
+example_counts = vectorizer.transform(examples)
+predictions = classifier.predict(example_counts)
+predictions
